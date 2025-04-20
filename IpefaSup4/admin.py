716c0b46028d581a658442c4cb097a07dc5ec35a
padeb185 <pierre-andre@hotmail.be>
2025-04-20 10:21:11 +0200

    first commit

---
+++ IpefaSup4/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Teacher, Student, Educator, Administrator, Section, Session, AcademicUE, UE, Registration, Participation
+
+
+
+
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Educator)
+admin.site.register(Administrator)
+admin.site.register(Section)
+admin.site.register(Session)
+admin.site.register(AcademicUE)
+admin.site.register(UE)
+admin.site.register(Registration)
+admin.site.register(Participation)
